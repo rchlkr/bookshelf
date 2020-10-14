00ac5bbc228c109f46104245cf7954cd4eb4b981
Rebecca Chalker <rebeccachalker@protonmail.com>
2020-10-14 12:55:28 -0500

    add localStorage

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let myLibrary = [];
+let myLibrary = localStorage.getItem("books")
+	? JSON.parse(localStorage.getItem("books"))
+	: [];
+
+localStorage.setItem("books", JSON.stringify(myLibrary));
+const data = JSON.parse(localStorage.getItem("books"));
 
 //constructor function
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
 }
 
 const submit = document.getElementById("submit");
@@ -13,102 +18,110 @@ submit.addEventListener("click", addBookToLibrary);
 const readRadio = document.getElementById("read");
 const unreadRadio = document.getElementById("unread");
 function addBookToLibrary() {
-  //populate constructor function values from input fields
-  let newTitle = document.getElementById("title").value;
-  let newAuthor = document.getElementById("author").value;
-  let newPages = document.getElementById("pages").value;
-  let newRead = document.getElementById("read");
+	//populate constructor function values from input fields
+	let newTitle = document.getElementById("title").value;
+	let newAuthor = document.getElementById("author").value;
+	let newPages = document.getElementById("pages").value;
+	let newRead = document.getElementById("read");
 
-  //determine readstatus based on checked/unchecked
-  if (newRead.checked === true) {
-    newRead.value = "&#x2714;";
-  } else {
-    newRead.value = "";
-  }
+	//determine readstatus based on checked/unchecked
+	if (newRead.checked === true) {
+		newRead.value = "&#x2714;";
+	} else {
+		newRead.value = "";
+	}
 
-  //create new book object, add it to myLibrary & create display card
-  let newBook = new Book(newTitle, newAuthor, newPages, newRead.value);
-  myLibrary.push(newBook);
-  createCard(newBook);
+	//create new book object, add it to myLibrary & create display card
+	let newBook = new Book(newTitle, newAuthor, newPages, newRead.value);
+	myLibrary.push(newBook);
+	localStorage.setItem("books", JSON.stringify(myLibrary));
 
-  //clear text fields after book added
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
-  document.getElementById("pages").value = "";
-  readRadio.checked = false;
-  unreadRadio.checked = false;
+	createCard(newBook);
+
+	//clear text fields after book added
+	document.getElementById("title").value = "";
+	document.getElementById("author").value = "";
+	document.getElementById("pages").value = "";
+	readRadio.checked = false;
+	unreadRadio.checked = false;
 }
 
 const section = document.querySelector("#section");
 function createCard(newBook) {
-  //create card when new book added, populate it with book details
-  const card = document.createElement("article");
-  section.appendChild(card);
-  card.classList.add("card");
-  card.innerHTML = `<strong>${newBook.title}</strong><br><br>${newBook.author}<br><br>${newBook.pages} pages<br><br>${newBook.read}`;
-  card.style.wordWrap = "break-word";
-  card.style.color = "hsl(0, 0%, 45%)";
+	//create card when new book added, populate it with book details
+	const card = document.createElement("article");
+	section.appendChild(card);
+	card.classList.add("card");
+	card.innerHTML = `<strong>${newBook.title}</strong><br><br>${newBook.author}<br><br>${newBook.pages} pages<br><br>${newBook.read}`;
+	card.style.wordWrap = "break-word";
+	card.style.color = "hsl(0, 0%, 45%)";
 
-  //set IDs for each card
-  let setCardIds = () => {
-    let cardId = document.getElementsByClassName("card");
-    for (i = 0; i < cardId.length; i++) {
-      cardId[i].id = i;
-    }
-  };
-  setCardIds();
+	//set IDs for each card
+	let setCardIds = () => {
+		let cardId = document.getElementsByClassName("card");
+		for (i = 0; i < cardId.length; i++) {
+			cardId[i].id = i;
+		}
+	};
+	setCardIds();
 
-  //create remove/read buttons, default visibility is hidden
-  let remove = document.createElement("button");
-  let markRead = document.createElement("button");
-  remove.classList.add("remove");
-  markRead.classList.add("markread");
-  card.appendChild(remove);
-  card.appendChild(markRead);
-  remove.textContent = "remove";
-  markRead.innerHTML = "&#x2714;";
-  remove.style.visibility = "hidden";
-  markRead.style.visibility = "hidden";
+	//create remove/read buttons, default visibility is hidden
+	let remove = document.createElement("button");
+	let markRead = document.createElement("button");
+	remove.classList.add("remove");
+	markRead.classList.add("markread");
+	card.appendChild(remove);
+	card.appendChild(markRead);
+	remove.textContent = "remove";
+	markRead.innerHTML = "&#x2714;";
+	remove.style.visibility = "hidden";
+	markRead.style.visibility = "hidden";
 
-  //when mouse hovers over card, show remove/read
-  let showOptions = () => {
-    setCardIds(); //updates card ids after removing book
-    let index = card.id;
-    remove.style.visibility = "visible";
-    myLibrary.forEach((book) => {
-      if (myLibrary[index].read === "&#x2714;") {
-        markRead.style.visibility = "hidden";
-      } else {
-        markRead.style.visibility = "visible";
-      }
-    });
-    //always hide checkmark if book is already read
-  };
-  //when mouse exits card, hide remove/edit
-  const hideOptions = () => {
-    remove.style.visibility = "hidden";
-    markRead.style.visibility = "hidden";
-  };
+	//when mouse hovers over card, show remove/read
+	let showOptions = () => {
+		setCardIds(); //updates card ids after removing book
+		let index = card.id;
+		myLibrary.forEach((book) => {
+			if (myLibrary[index].read === "&#x2714;") {
+				remove.style.visibility = "visible";
+				markRead.style.visibility = "hidden";
+			} else {
+				remove.style.visibility = "visible";
+				markRead.style.visibility = "visible";
+			}
+		});
+		//always hide checkmark if book is already read
+	};
+	//when mouse exits card, hide remove/edit
+	const hideOptions = () => {
+		remove.style.visibility = "hidden";
+		markRead.style.visibility = "hidden";
+	};
 
-  //remove button deletes card from section and myLibrary
-  let removeBook = () => {
-    let index = card.id;
-    myLibrary.splice(index, 1);
-    remove.parentElement.remove();
-  };
-  //clicking read changes card value and array value
-  let readStatus = document.getElementById("readStatus");
-  let changeStatus = () => {
-    let index = card.id;
-    myLibrary[index].read = "&#x2714;";
-    card.innerHTML = `<strong>${newBook.title}</strong><br><br>${newBook.author}<br><br>${newBook.pages} pages<br><br>&#x2714`;
-  };
+	//remove button deletes card from section and myLibrary
+	let removeBook = () => {
+		let index = card.id;
+		myLibrary.splice(index, 1);
+		remove.parentElement.remove();
+	};
+	//clicking read changes card value and array value
+	let readStatus = document.getElementById("readStatus");
+	let changeStatus = () => {
+		let index = card.id;
+		myLibrary[index].read = "&#x2714;";
+		card.innerHTML = `<strong>${newBook.title}</strong><br><br>${newBook.author}<br><br>${newBook.pages} pages<br><br>&#x2714`;
+	};
 
-  card.addEventListener("mouseover", showOptions);
-  card.addEventListener("mouseout", hideOptions);
-  remove.addEventListener("click", removeBook);
-  markRead.addEventListener("click", changeStatus);
+	card.addEventListener("mouseover", showOptions);
+	card.addEventListener("mouseout", hideOptions);
+	remove.addEventListener("click", removeBook);
+	markRead.addEventListener("click", changeStatus);
 }
+
+//create card for each book in localStorage
+data.forEach((book) => {
+	createCard(book);
+});
 
 //when addbook is clicked, book info popup appears
 let addBtn = document.getElementById("addbook");
@@ -120,41 +133,43 @@ closeButton.addEventListener("click", showModal);
 window.addEventListener("click", windowOnClick);
 
 function showModal() {
-  modal.classList.toggle("showmodal");
+	modal.classList.toggle("showmodal");
 }
 
 function windowOnClick(event) {
-  if (event.target === modal) {
-    showModal();
-  }
-  if (event.target === submit) {
-    showModal();
-  }
+	if (event.target === modal) {
+		showModal();
+	}
+	if (event.target === submit) {
+		showModal();
+	}
 }
 
 //introDiv appears on page load, disappears after addbook is clicked
 let introDiv = document.getElementById("arrowdiv");
 window.addEventListener("load", function () {
-  introDiv.style.visibility = "visible";
+	//hide if localStorage is populated
+	if (!localStorage.getItem("book")) {
+		introDiv.style.visibility = "hidden";
+	} else {
+		introDiv.style.visibility = "visible";
+	}
 });
 addBtn.addEventListener("click", function () {
-  introDiv.style.visibility = "hidden";
+	introDiv.style.visibility = "hidden";
 });
 
 //credit info appears on hover of info icon, disappears on mouseout
 const info = document.getElementById("credit");
 const flatIcon = document.getElementById("flaticon");
 info.addEventListener("mouseover", function () {
-  flatIcon.style.visibility = "visible";
+	flatIcon.style.visibility = "visible";
 });
 info.addEventListener("mouseout", function () {
-  flatIcon.style.visibility = "hidden";
+	flatIcon.style.visibility = "hidden";
 });
 
 /*
-todo:
-  localstorage
-
 ideas:
   only show title on small card, clicking expands card to show all details
   group by read/unread
